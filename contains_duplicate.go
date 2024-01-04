1d9491221c1cbc7c8d98ead508757cf6b51a6e77
Murillo Nahás <murillonahas2003@gmail.com>
2024-01-03 21:55:39 -0300

    feat: contains duplicate WIP

---
+++ contains_duplicate.go
@@ -0,0 +1,10 @@
+// Given an integer array nums, return true if any value appears at least twice in the array,
+// and return false if every element is distinct.
+
+package main
+
+func containsDuplicate(nums []int) bool {
+	
+ // todo
+ return true
+}
